9ee76211a336447073bc08e370e822f00da6f56e
Camilo Ducuara <animafoxy05@gmail.com>
2022-04-28 10:20:10 -0500

    future test

---
+++ parking/src/test/java/com/proyectobackend/parking/ParkingDtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectobackend.parking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingDtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
